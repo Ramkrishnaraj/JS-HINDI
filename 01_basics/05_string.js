1a14918554bd326ec1cabf0348671633fd5d4329
RAMKRISHNA MAHTO <105748409+Ramkrishnaraj@users.noreply.github.com>
2023-12-04 08:34:32 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -2,4 +2,10 @@ const name="Radha"
 const repoNum= 50
 //console.log(name + repoNum + " Value")
 
-console.log(` My name is &{Ramkrishna} and repoNumer is ${50}`);
+console.log(`My name is &{Ramkrishna} and repoNumer is ${50}`);
+
+
+const gameName = new String('Ramkrishna')
+console.log(gameName[0])
+console.log(gameName.length)
+
