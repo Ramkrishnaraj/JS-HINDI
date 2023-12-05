24dd85a1ea23d244736f88f66f201bb89a47ac3f
RAMKRISHNA MAHTO <105748409+Ramkrishnaraj@users.noreply.github.com>
2023-12-05 05:42:19 +0000

    Num And Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,29 @@
+const Score=100
+// console.log(Score)
+
+const num = new Number(100)
+// console.log(num)
+// console.log(num.toString().length)
+// console.log(num.toFixed(2))
+
+const otherNumber=123.584
+// console.log(otherNumber.toPrecision(4))
+
+const NewNumber=100000000
+// console.log(NewNumber.toLocaleString('en-In'))
+
+//  ++++++++++++++++++++ Maths ++++++++++++++++++++++
+
+
+// console.log(Math)
+// console.log(Math.abs(-4.5))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.9))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+const min=12
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
