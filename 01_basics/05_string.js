11cc8480bc1fee5b4c57b07835e85e08846d9cd1
RAMKRISHNA MAHTO <105748409+Ramkrishnaraj@users.noreply.github.com>
2023-12-05 04:24:41 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,11 +1,19 @@
-const name="Radha"
+const name="Radha "
 const repoNum= 50
-//console.log(name + repoNum + " Value")
-
+console.log(name + repoNum + " Value")
 console.log(`My name is &{Ramkrishna} and repoNumer is ${50}`);
-
-
-const gameName = new String('Ramkrishna')
+const gameName =new String( "Ramkrishna")
 console.log(gameName[0])
 console.log(gameName.length)
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('a'))
+const anotherString = gameName.substring(0 , 4)
+console.log(anotherString)
+const SString =gameName.slice(-8 , 4)
+console.log(SString)
+const newanotherstring="   Ramkrishna   "
+console.log(newanotherstring)
+console.log(newanotherstring.trim())
+console.log(newanotherstring.trimStart())
+console.log(newanotherstring.trimEnd())
 
