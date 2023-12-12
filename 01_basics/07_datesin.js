e12208a8c2c8fae3e7753ebe0324b91575576b8d
RAMKRISHNA MAHTO <105748409+Ramkrishnaraj@users.noreply.github.com>
2023-12-12 02:54:00 +0000

    Dates

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,10 @@
+let MyDate=new Date()
+console.log(MyDate.toString())
+console.log(MyDate.toISOString())
+console.log(MyDate.toJSON())
+console.log(MyDate.toLocaleDateString())
+
+console.log(MyDate.toLocaleString())
+console.log(MyDate.toString())
+console.log(MyDate.toTimeString())
+console.log(MyDate.getDate())
