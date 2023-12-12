00417b715392787dcda87188f56f4be1491e174d
RAMKRISHNA MAHTO <105748409+Ramkrishnaraj@users.noreply.github.com>
2023-12-12 03:59:29 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,39 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes =["Ranbir, Salman, Shaktiman"]
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1])
+
+// Array methods***************
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+
+
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr);
+
+
+
+// slice, splice
+console.log("A =", myArr);
+const r1 = myArr.slice(1, 3)
+
+console.log(r1)
+console.log("B =", myArr)
+
+const r2 =myArr.splice(1, 3)
+console.log("C =", myArr)
+console.log(r2)
+ 
+
