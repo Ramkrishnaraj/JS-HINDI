2d2850367f1a570cb4dd974cff883d717fe65bec
RAMKRISHNA MAHTO <105748409+Ramkrishnaraj@users.noreply.github.com>
2023-12-21 20:48:17 +0000

    Object

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,24 @@
+// const tinderuser=new Object()
+const tinderuser= {}
+tinderuser.id=123
+tinderuser.name="Ramkrishna"
+tinderuser.mob=7296031836
+tinderuser.IsLoggedIn=false
+// console.log(tinderuser)
+const regularUser={
+    email:"trfhf@gmailcom",
+    fullname:{
+        Userfullname:{
+        firstname:"Ramkrishna",
+        lastname:"Mahto"
+
+    }
+}
+}
+console.log(regularUser.fullname)
+console.log(regularUser.email)
+console.log(regularUser.IsLoggedIn)
+console.log(regularUser.fullname.Userfullname.firstname)
+
+
+
